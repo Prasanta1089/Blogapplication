6414ed946b027d56d5c63ed34aef3b5e57762edf
Prasanta <prasantanimalpuri@gmail.com>
2023-08-18 11:54:51 +0530

    change

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c^7$tn%zxl7emre()^2mko@s($t^j3l&8%o4@&6*)7fy25s6qs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app', '.now.sh', '127.0.0.1' , 'localhost']
 
 
 # Application definition
@@ -124,9 +124,12 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-STATICFILES_DIRS=[
-    BASE_DIR/"static",
-]
+
+
+#STATICFILES_DIRS=[BASE_DIR/"static",]
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
