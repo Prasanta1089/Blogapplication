a5b00c03c226b34c4cae4167bf5b3105f110b3dd
Prasanta <prasantanimalpuri@gmail.com>
2023-08-19 11:37:11 +0530

    change

--- build_files.sh
+++
@@ -1,2 +0,0 @@
-pip install -r requirements.txt
-Python3 3.10.4 manage.py collectstatic
--- iCoder/settings.py
+++ iCoder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c^7$tn%zxl7emre()^2mko@s($t^j3l&8%o4@&6*)7fy25s6qs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vercel.app', '.now.sh', '127.0.0.1' , 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -126,10 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 
-#STATICFILES_DIRS=[BASE_DIR/"static",]
+STATICFILES_DIRS=[BASE_DIR/"static",]
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
